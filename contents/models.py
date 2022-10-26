5834c10a390dbc9e93cf602118f0ffb814b82d57
hanmariyang <hanmari.code@gmail.com>
2022-10-26 20:01:29 +0900

    [업데이트] Feed 모델 추가
    [모델 필드 부분]
    title = models.CharField(max_length=30)
    category = models.CharField(max_length=30)
    image = models.ImageField(default="default.png", upload_to="feed_images/")
    content = models.TextField()
    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
    like_authors = models.ManyToManyField(UserModel, related_name='like_posts')
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)

--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import UserModel
+
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    image = models.ImageField(default="default.png", upload_to="feed_images/")
+    content = models.TextField()
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    like_authors = models.ManyToManyField(UserModel, related_name='like_posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.title)
